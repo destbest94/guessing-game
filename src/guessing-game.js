d1e7d6ea116778e3f4149ad06f11df9b26c79c08
Abdukhalilov Bobur <destbest94@gmail.com>
2019-10-02 22:30:49 +0300

    update: resolved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.result = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.result = Math.ceil((max + min) / 2);
     }
 
     guess() {
-
+        return this.result;
     }
 
     lower() {
+        this.max = this.result;
+        let range = this.max + this.min;
 
+        this.result = Math.ceil((range) / 2);
     }
 
     greater() {
+        this.min = this.result;
+        let range = this.max + this.min;
 
+        this.result = Math.ceil((range) / 2);
     }
 }
 
